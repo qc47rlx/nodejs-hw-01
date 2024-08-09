bfb9b396d7f07e217aa11130acb6dae2c5e4ab4a
Anton Postnov <4qc7@proton.me>
2024-08-09 11:35:47 +0300

    done

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,5 +1,4 @@
-const PATH_DB = './src/db/db.json';
+import path from 'node:path';
+const pathToWorkDir = path.join(process.cwd());
+export const PATH_DB = path.join(pathToWorkDir, 'src', 'db', 'db.json');
 
-export default {
-  PATH_DB
-};
